ef5c2b200bd762df361a28858812789c249cd9cc
Yago <uo295464@uniovi.es>
2025-12-02 14:30:43 +0100

    Primera función HTTP, no funciona

--- app/src/main/java/es/uniovi/converter/MainActivity.kt
+++ app/src/main/java/es/uniovi/converter/MainActivity.kt
@@ -8,9 +8,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    val euroToDollar = 1.16
+    var euroToDollar = 1.16
     lateinit var editTextEuros: EditText
     lateinit var editTextDollars: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         editTextEuros = findViewById(R.id.editTextEuros)
         editTextDollars = findViewById(R.id.editTextDollars)
+        fetchExchangeRate()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -45,6 +51,24 @@ class MainActivity : AppCompatActivity() {
             return
         }
         destination.setText((value * factor).toString())
+    }
+
+    private fun fetchExchangeRate() {
+        val url = URL("https://api.frankfurter.app/latest?from=EUR&to=USD")
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        val inputStream = connection.inputStream
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val response = StringBuilder()
+        var line: String?
+        while (reader.readLine().also { line = it } != null) {
+            response.append(line)
+        }
+        reader.close()
+        val jsonResponse = response.toString()
+        val jsonObject = JSONObject(jsonResponse)
+        val rates = jsonObject.getJSONObject("rates")
+        euroToDollar = rates.getDouble("USD")
     }
 
 
